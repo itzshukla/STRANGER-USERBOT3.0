98157bfda79c0fe2b3d6f76f473232e246602835
STRANGER <147219905+itzshukla@users.noreply.github.com>
2023-11-20 20:12:12 +0530

    Update multisess.py

--- multisess.py
+++ multisess.py
@@ -16,9 +16,9 @@ try:
             sess = str(s)
             if whom:
                 id = RiZoeL.get_users(whom).id
-                RiZoeL.send_message(id, f"**Pyrogram String Session** \n\n `{sess}` \n\n © @RiZoeLX")
+                RiZoeL.send_message(id, f"**Pyrogram String Session** \n\n `{sess}` \n\n © @SHIVANSH39")
             else:
-                RiZoeL.send_message("me", f"**Pyrogram String Session** \n\n `{sess}` \n\n © @RiZoeLX")
+                RiZoeL.send_message("me", f"**Pyrogram String Session** \n\n `{sess}` \n\n © @SHIVANSH39")
        
      if whom:
            print(f"All Session has been sent to {whom}")
