94b7939cdc05f4f2baa82472a70bf5732b0771a6
STRANGER <147219905+itzshukla@users.noreply.github.com>
2024-01-29 11:42:26 +0530

    Update __init__.py

--- SpamX/modules/__init__.py
+++ SpamX/modules/__init__.py
@@ -8,9 +8,9 @@ import platform
 __version__ = __version__
 
 
-ping_msg = PING_MSG if PING_MSG else "𝗦𝗧𝗥𝗔𝗡𝗚𝗘𝗥"
+ping_msg = PING_MSG if PING_MSG else "⚡️𝗦𝗧𝗥𝗔𝗡𝗚𝗘𝗥⚡️"
 pic = ALIVE_PIC if ALIVE_PIC else "https://telegra.ph/file/aa4bf1e57d11fb75b602e.jpg"
-amsg = ALIVE_MSG if ALIVE_MSG else"𝗦𝗧𝗥𝗔𝗡𝗚𝗘𝗥 𝗦𝗣𝗔𝗠"
+amsg = ALIVE_MSG if ALIVE_MSG else"⚡️𝗦𝗧𝗥𝗔𝗡𝗚𝗘𝗥 𝗦𝗣𝗔𝗠⚡️"
 
 try:
    sah = RiZoeL.get_users(OWNER_ID)
@@ -22,10 +22,10 @@ class Alive:
      Pic = pic
      
      msg = f"""
-      **[☆𝗦𝗧𝗥𝗔𝗡𝗚𝗘𝗥☆](https://t.me/mastiwithfriendsx)
+      **[⚡️𝗦𝗧𝗥𝗔𝗡𝗚𝗘𝗥⚡️](https://t.me/SHIVANSH474)
 ◈ •━━━━━★✦♡✦★━━━━━• ◈ 
 
-➪ **𝗠ᴀsᴛᴇʀ:** [☆𝗦𝗛𝗜𝗩𝗔𝗡𝗦𝗛☆](https://t.me/ITSZ_SHIVANSH)
+➪ **𝗠ᴀsᴛᴇʀ:** [⚡️𝗦𝗛𝗜𝗩𝗔𝗡𝗦𝗛⚡️](https://t.me/ITSZ_SHIVANSH)
 
 ➪ **𝗣ʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ:**`{platform.python_version()}`
 
